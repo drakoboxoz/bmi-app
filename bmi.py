cb3269de8fc4efddacbc5cc535e721c6dc5229f2
Drakoboxoz <captainbungbung@gmail.com>
2025-04-24 11:58:10 +0700

    Create bmi.py
    this might cause emotional trauma

---
+++ bmi.py
@@ -0,0 +1,32 @@
+import streamlit as s
+s.title("bmi calculator")
+h=0.00
+w=0
+w = s.number_input(f"please type your weight",min_value=1.00,max_value=500.00) 
+h = s.number_input(f"now please put your height (cm)",step=0.1,min_value=1.00,max_value=450.00)
+bmi=w/(h/100)**2
+if bmi<18.5:
+    s.info(f"{bmi}")
+    s.info("underweight")
+    s.warning("You're so skinny, when you turn sideways, you disappeared.")
+    s.image("C:/Users/Captain/Pictures/Screenshots/skinny.png.png")
+elif bmi<23:
+    s.info(f"{bmi}")
+    s.info("healthy")
+    s.success("keep up")
+    s.image("C:/Users/Captain/Pictures/Screenshots/normal.png")
+elif bmi<25:
+    s.info(f"{bmi}")
+    s.info("overweight")
+    s.warning("diet please?")
+    s.image("C:/Users/Captain/Pictures/Screenshots/chubby.png")
+elif bmi<30:
+    s.info(f"{bmi}")
+    s.info("fat")
+    s.error("you make a sumo wrestler look like a supermodel")
+    s.image("C:/Users/Captain/Pictures/Screenshots/brainrot.png")
+else:
+    s.info(f"{bmi}")
+    s.info("very fat... infact,u r caseoh reincarnate")
+    s.error("You're an absolute gluttonous beast, and the only exercise you get is lifting a fork to your mouth,People mistake you for a planet because of the gravitational pull you have on their food,You're the reason they invented double doors!,You're so fat the only letters of the alphabet you know are KFC.")
+    s.image("C:/Users/Captain/Pictures/Screenshots/caseoh.png")
